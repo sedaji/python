203bb355a8a4235bd323c88defe13993bad2f9d8
ronnie wong <44921067+sedaji@users.noreply.github.com>
2019-06-01 17:26:01 -0500

    Update chapter 6 - Tables.py

--- automatedbook/chapter 6 - Tables.py
+++ automatedbook/chapter 6 - Tables.py
@@ -1,12 +1,17 @@
+#! /usr/bin/python3
+
 tableData = [['apples', 'oranges', 'cherries', 'banana'], ['Alice', 'Bob', 'Carol', 'David'], ['dogs', 'cats', 'moose', 'goose']]
 
 L = [0,0,0] 
 for i in range(3): 
   for j in range(4): 
-      listLen = len(tableData[i][j]) if L[i] < listLen: L[i] = listLen
+      listLen = len(tableData[i][j]) 
+      if L[i] < listLen:
+      		L[i] = listLen
 
 def printTable(): 
   for i in range(4): 
     for j in range(3): 
-      print((tableData[j][i]).rjust(L[j]+2,'.'), end=' ') print()
+      print((tableData[j][i]).rjust(L[j]+2,'.'), end=' ') 
+    print()
 printTable()
