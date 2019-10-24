e4e3f7e1ffd8115b51c52d61702e9e2bdc170ca9
Ronnie <44921067+sedaji@users.noreply.github.com>
2019-10-24 02:10:42 -0500

    randomized

--- functions/circleGen.py
+++ functions/circleGen.py
@@ -1,9 +1,15 @@
 #python 3.5.2
 import math
-length = 60
-height = 60
-thickness = .4
-circleChar = "++"
+import random
+length = 70
+height = 70
+thickness = .325
+
+randString = "!$%^&*_+{}|():,./;'[]\-=_<>?"
+randString2 = "__"
+listLen = len(randString)
+listLen2 = len(randString2)
+
 
 centerX = length/2
 centerY = height/2
@@ -17,10 +23,11 @@ def DistanceForm(x1,y1):
 
 for i in range(height+1):
     for j in range(length+1):
-        
+        circleChar = randString[random.randint(0,(listLen-1))] + randString[random.randint(0,(listLen-1))]
+        circleChar2 = randString2[random.randint(0,(listLen2-1))] + randString2[random.randint(0,(listLen2-1))]
         radius = (DistanceForm(i,j))
-        if radius > (length/2-thickness) and radius < (height/2+thickness):
+        if radius < (height/2+thickness):
             print(circleChar,end='')
         else:
-            print("  ",end='')
+            print(circleChar2,end='')
     print()
